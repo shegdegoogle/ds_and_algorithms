4b93775cdf8329b9803a781dda7bb36b0f0b41d3
shegdegoogle <78445277+shegdegoogle@users.noreply.github.com>
2021-02-02 17:31:34 -0600

    Update PostOrder_Traversal_Without_Recursion.cs

--- PostOrder_Traversal_Without_Recursion.cs
+++ PostOrder_Traversal_Without_Recursion.cs
@@ -38,3 +38,46 @@
         }
         return res;
     }    
+/// using HashSet
+
+
+
+    /*
+        public class TreeNode {
+            public int val;
+            public TreeNode left_ptr;
+            public TreeNode right_ptr;
+        }
+    */    
+    
+    // Complete the function below.
+    public static List<int> postorder_traversal(TreeNode root){
+        if (root == null)
+            return new List<int>();
+       
+        Stack<TreeNode> stk = new Stack<TreeNode>();
+        List<int> res = new List<int>();
+        HashSet<TreeNode> hs = new HashSet<TreeNode>();
+        stk.Push(root);
+        TreeNode curr = root;
+        while (stk.Count() >0)
+        {
+            curr = stk.Peek();
+            if(curr.left_ptr != null && !hs.Contains(curr.left_ptr))
+            {
+                stk.Push(curr.left_ptr);
+                hs.Add(curr.left_ptr);
+            }
+            else if ( curr.right_ptr != null && !hs.Contains(curr.right_ptr))
+            {
+                stk.Push(curr.right_ptr);
+                hs.Add(curr.right_ptr);
+            }
+            else
+            { 
+                res.Add(stk.Pop().val);
+            }
+            
+        }
+        return res;
+    }    
