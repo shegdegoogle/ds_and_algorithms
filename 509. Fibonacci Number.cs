342ecdb2c551058ae5cafdd7a626f5552412889e
shegdegoogle <78445277+shegdegoogle@users.noreply.github.com>
2021-02-15 14:44:06 -0600

    Create 509. Fibonacci Number.cs
    509. Fibonacci Number DP solution

---
+++ 509. Fibonacci Number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Fib(int n) {
+        if(n ==0)
+            return 0;
+        int[] dp = new int[n+1];
+        
+        //base cases
+        dp[0] =0;
+        dp[1] =1;
+        
+        for(int i =2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+    }
+}
