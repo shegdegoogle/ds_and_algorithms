4c91febf40b5fe34e49447f4f707f1012431ae9d
shegdegoogle <78445277+shegdegoogle@users.noreply.github.com>
2021-02-15 14:55:16 -0600

    Create 70. Climbing Stairs.cs
    70. Climbing Stairs DP solution

---
+++ 70. Climbing Stairs.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if( n ==0)
+            return 1;
+        int[] dp = new int[n+1];
+        
+        //base case
+        dp[0] =1;
+        dp[1] =1;
+        
+        for(int i =2; i<=n;i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
